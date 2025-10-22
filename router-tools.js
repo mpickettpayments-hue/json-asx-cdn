a40cd87ede6bc937155cd53cb4a8067fb2a28282
DEVMICRO.APP <m.pickett.payments@gmail.com>
2025-10-21 19:37:38 -0700

    Update router-tools.js

--- router-tools.js
+++ router-tools.js
@@ -1,9 +1,8 @@
 // router-tools.js
 export function redirectToUserDomain(uid) {
-  const base = window.location.protocol;
-  const domain = window.location.hostname;
-  const root = domain.replace(/^www\./,'');
-  const parts = root.split('.');
-  const mainDomain = parts.slice(parts.length-2).join('.');
-  window.location.href = `${base}//${uid}.${mainDomain}/`;
+  const proto = window.location.protocol;
+  const host = window.location.hostname.replace(/^www\./,'');
+  const parts = host.split('.');
+  const main = parts.slice(parts.length-2).join('.');
+  window.location.href = `${proto}//${uid}.${main}/`;
 }
